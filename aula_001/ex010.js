4de361b8f306c5b2c7e3b30b749da9c3d84c9988
Heitor <49591729+hectorclakrag@users.noreply.github.com>
2023-06-16 21:03:44 +0100

    estudando condicoes simples

---
+++ aula_001/ex010.js
@@ -0,0 +1,7 @@
+var pais = 'Franca'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Voce e Brasileiro!')
+} else {
+    console.log('Voce e considerado um estrangeiro!')
+}
