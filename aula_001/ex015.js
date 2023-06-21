e4eb481a8bba88058ede14069efe1b48a8919593
Heitor <49591729+hectorclakrag@users.noreply.github.com>
2023-06-21 14:38:41 +0100

    Update ex015.js

--- aula_001/ex015.js
+++ aula_001/ex015.js
@@ -1,5 +1,16 @@
-var c = 1;
-while (c <= 10) {
-    console.log('Ola')
-    c++
+// var c = 1;
+// do {
+//     console.log(`Passo ${c}`)
+//     c++
+// } while (c <= 3)
+
+
+// var c = 1;
+// while (c <= 3) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+for  (var c=1; c<=5; c++) {
+    console.log(`Passo ${c}`)
 }
